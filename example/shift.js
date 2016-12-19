13891e5299c536e7a39b36888fc207fd83be4872
brunolandowski <bruno.landowski@gmail.com>
2016-12-19 22:45:38 +0100

    'reademe'

--- example/shift.js
+++ example/shift.js
@@ -1,3 +1,7 @@
+/*!
+ * Shift 1.0.0
+ * https://github.com/brunolandowski/shift
+ */
 (function($) {
         $.fn.shift = function(params) {
         
@@ -37,3 +41,7 @@
         return this;
         };
 })(jQuery);
+
+console.log("%cNexus plugin made by Bruno Landowski", "background: #333; color: #bdbd0a; padding: 5px 10px;");
+console.log("%chttp://brunolandowski.fr", "color:#333");
+console.log("%chttps://github.com/brunolandowski", "color:#333");
