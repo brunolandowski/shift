1c1c755d8c350347cfbc27fe9c648592a65962dd
brunolandowski <bruno.landowski@gmail.com>
2016-12-19 22:33:01 +0100

    'reademe'

--- example/shift.js
+++ example/shift.js
@@ -1,18 +1,28 @@
 (function($) {
-        // définition du plugin jQuery 
-        $.fn.reverseText = function(params) {
+        $.fn.shift = function(params) {
         
-        $(this).each(function( index ) {
-                $('section').on('mousewheel', function(event) {
-                        console.log(this);
-                        var st = $(this).scrollTop(),
-                        inn = $(this)[0].scrollHeight,
-                        hg = $(this).height(),
-                        cl = inn - hg,
-                        di = (st / cl) * 100;
+                $(this).each(function( index ) {
+                        // Get selector name
+                        var selecteur = $(this)[0].tagName;
+                        
+                        // Event when mouse-wheel is activated
+                        $(this).on('mousewheel', function(event) {
 
-                        $('section').not(this).each(function(){
+                                // Get scrolled pixel
+                                var     st = $(this).scrollTop(),
 
+                                        // Get the window height
+                                        inn = $(this)[0].scrollHeight,
+                                        
+                                        // Get height of the element being scrolled        
+                                        hg = $(this).height(),
+                                        
+
+                                        // Window height minus element height
+                                        cl = inn - hg,
+                                        di = (st / cl) * 100;
+
+                        $(selecteur).not(this).each(function(){
                                 var     inn2 = $(this)[0].scrollHeight,
                                         hg2 = $(this).height(),
                                         cl2 = inn2 - hg2,
@@ -22,12 +32,8 @@
                         });
    
                 });
-        });     
-
-
-        
-        
-
+        });
+        // Prevend conflicts with this     
         return this;
         };
 })(jQuery);
